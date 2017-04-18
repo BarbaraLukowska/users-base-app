28f51c839396a1a6b175600da3c1a079784c60e5
Barbara Łukowska <barbara.m.lukowska@gmail.com>
2017-04-18 16:26:20 +0200

    userbase.js added and created UserRow and UserList

--- js/app.jsx
+++ js/app.jsx
@@ -1,12 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import usersbase from './usersbase';
 // import "../scss/style.scss";
 
 document.addEventListener('DOMContentLoaded', function(){
 
+    class UserList extends React.Component {
+    constructor(props){
+      super(props);
+      this.state = {
+        users: this.props.users
+      }
+    }
+    render(){
+      const listItem = this.state.users.map( (element) => {
+        return <UserRow
+          key = {element.email}
+          number={element.id}
+          userName={element.name}
+          userEmail={element.email}/>
+      });
+      return <table>
+        <thead>
+          <tr>
+            <th>Lp.</th>
+            <th>User</th>
+            <th>email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {listItem}
+        </tbody>
+      </table>
+    }
+  }
+  class UserRow extends React.Component {
+
+  render(){
+    return <tr>
+            <td>
+              {this.props.number}
+            </td>
+            <td>
+              {this.props.userName}
+            </td>
+            <td>
+              {this.props.userEmail}
+            </td>
+            <td>
+              <input type="button" value="X"/>
+            </td>
+          </tr>
+  }
+}
+
   class App extends React.Component {
     render(){
       return <div>
+        <UserList users={usersbase}/>
       </div>
     }
   }
---
+++ js/usersbase.js
@@ -0,0 +1,17 @@
+module.exports = [
+    {
+        id: 1,
+        name: 'John Novicky',
+        email: 'john.novicky@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Ann Lois',
+        email: 'annlois29@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Anthony Rowland',
+        email: 'anthony79@gmail.com'
+    }
+];
